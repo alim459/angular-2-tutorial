ee11644ced3c8515cabd11ccbfa23ec1b35d4395
[alvin] [lim] <alvin.lim@visioncritical.com>
2016-11-09 10:02:34 -0800

    delete committed node modules

--- app/products/product-list.component.js
+++ app/products/product-list.component.js
@@ -15,6 +15,7 @@ var ProductListComponent = (function () {
         this.imageWidth = 50;
         this.imageMargin = 2;
         this.showImage = false;
+        this.listFilter = 'cart';
         this.products = [
             {
                 "productId": 1,
@@ -71,10 +72,16 @@ var ProductListComponent = (function () {
     ProductListComponent.prototype.toggleImage = function () {
         this.showImage = !this.showImage;
     };
+    ProductListComponent.prototype.ngOnInit = function () {
+        console.log("Hello");
+    };
     ProductListComponent = __decorate([
         core_1.Component({
             selector: 'pm-products',
-            templateUrl: 'app/products/product-list.component.html'
+            templateUrl: 'app/products/product-list.component.html',
+            styleUrls: [
+                '/app/products/product-list.component.css'
+            ]
         }), 
         __metadata('design:paramtypes', [])
     ], ProductListComponent);
